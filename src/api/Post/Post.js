df0ad3e52cdc9a457482b7bd37c87427a38c4297
Lee Jeong Min <danmin20@khu.ac.kr>
2020-02-06 16:35:44 +0900

    Post.js

---
+++ src/api/Post/Post.js
@@ -0,0 +1,31 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  Post: {
+    book: ({ id }) => prisma.post({ id }).book(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
+    likes: ({ id }) => prisma.post({ id }).likes(),
+    isLiked: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [{ user: { id: user.id } }, { post: { id } }]
+      });
+    },
+    likeCount: parent =>
+      prisma
+        .likesConnection({
+          where: { post: { id: parent.id } }
+        })
+        .aggregate()
+        .count(),
+    commentCount: parent =>
+      prisma
+        .commentsConnection({
+          where: { post: { id: parent.id } }
+        })
+        .aggregate()
+        .count()
+  }
+};
