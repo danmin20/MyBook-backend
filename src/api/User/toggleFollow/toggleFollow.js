c8602e8417bd06f841146b25d677f77c68e80245
Lee Jeong Min <danmin20@khu.ac.kr>
2020-02-07 23:28:52 +0900

    toggle follow

---
+++ src/api/User/toggleFollow/toggleFollow.js
@@ -0,0 +1,46 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    toggleFollow: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { id } = args;
+      const { user } = request;
+      try {
+        const existingFollow = await prisma.$exists.user({
+          AND: [{ id }, { followers_some: { id: user.id } }]
+        });
+        if (existingFollow) {
+          await prisma.updateUser({
+            where: {
+              id: user.id
+            },
+            data: {
+              following: {
+                disconnect: {
+                  id
+                }
+              }
+            }
+          });
+        } else {
+          await prisma.updateUser({
+            where: {
+              id: user.id
+            },
+            data: {
+              following: {
+                connect: {
+                  id
+                }
+              }
+            }
+          });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
+};
