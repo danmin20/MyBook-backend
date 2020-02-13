b31c75658f4960bed7f3ff366d5600a2373aff99
Lee Jeong Min <danmin20@khu.ac.kr>
2020-02-14 01:34:35 +0900

    pagination

--- src/api/Post/seePostDB/seePostDB.js
+++ src/api/Post/seePostDB/seePostDB.js
@@ -2,8 +2,10 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    seePostDB: async () => {
+    seePostDB: async (_, args) => {
       return prisma.posts({
+        first: args.first,
+        skip: args.offset,
         orderBy: "createdAt_DESC"
       });
     }
