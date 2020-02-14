223f1496b87077154ea5bec8b856372d838e76ef
Lee Jeong Min <danmin20@khu.ac.kr>
2020-02-14 14:46:52 +0900

    build

--- build/env.js
+++ build/env.js
@@ -2,10 +2,6 @@
 
 var _dotenv = _interopRequireDefault(require("dotenv"));
 
-var _path = _interopRequireDefault(require("path"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-_dotenv["default"].config({
-  path: _path["default"].resolve(__dirname, ".env")
-});
+_dotenv["default"].config();
